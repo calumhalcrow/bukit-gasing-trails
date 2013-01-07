40414200ea94c32342df7ddcd5fbbc9f63285565
Calum Halcrow <calumhalcrow@gmail.com>
2013-01-07 19:29:11 +0800

    Add relationship between Way and Node to models.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,4 @@
 class Node < ActiveRecord::Base
   attr_accessible :lat, :lon, :osmid, :way_id
+  belongs_to :way, :inverse_of => :nodes
 end
--- app/models/way.rb
+++ app/models/way.rb
@@ -1,3 +1,4 @@
 class Way < ActiveRecord::Base
   attr_accessible :category, :name
+  has_many :nodes, :inverse_of => :way
 end
