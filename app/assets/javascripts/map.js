b6801a5fd335a8c5dbcbb5e5f263bea7e6214ec7
Calum Halcrow <calumhalcrow@gmail.com>
2013-01-07 19:49:32 +0800

    Display all Ways stored in DB on map.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,4 @@
-// TODO
-// - plot an OSM path.
-// plot OSM's park boundary.
-function initialize() {
+$(function () {
     var mapOptions = {
         center: new google.maps.LatLng(3.097943, 101.660782),
         zoom: 16,
@@ -10,46 +7,16 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-
-
-
-
-
-
-    Object.keys(coords.gpsRecordings).forEach(function(date) {
-        //if (date == '2012-10-06') {
-            //return;
-        //}
-        var recCoords = new Array();
-        coords.gpsRecordings[date].forEach(function(coord) {
-            var latLng = new google.maps.LatLng(coord.lat,coord.lng);
-
-            recCoords.push(latLng);
-
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map,
-                title: date+' '+coord.time
+    var request = $.ajax({
+        url: "/ways.json"
+    });
+    request.done(function(ways) {
+        ways.forEach(function(way) {
+            var coords = new Array();
+            way.nodes.forEach(function(node) {
+                coords.push(new google.maps.LatLng(node.lat, node.lon));
             });
+            (new google.maps.Polygon({ paths: coords })).setMap(map);
         });
-        var strokeColor = (date == '2012-10-06') ? '#ff0000' : '#00ff00';
-        var walk = new google.maps.Polyline({
-            path: recCoords,
-            strokeColor: strokeColor,
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        });
-        walk.setMap(map);
     });
-
-
-    ['tamanRimba', 'vilaDbkl', 'graveyard'].forEach(function(place) {
-        var placeCoords = new Array();
-        coords[place].forEach(function(coord) {
-            placeCoords.push(new google.maps.LatLng(coord.lat, coord.lng));
-        });
-        (new google.maps.Polygon({ paths: placeCoords })).setMap(map);
-    });
-}
-
-window.onload = initialize;
+});
