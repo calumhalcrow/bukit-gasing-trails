367becd31532cd9b7cda331c874b235601286a96
Calum Halcrow <calumhalcrow@gmail.com>
2013-01-20 14:10:15 +0800

    Way.fetch_from_OSM.

--- Gemfile
+++ Gemfile
@@ -37,3 +37,6 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'ruby-debug'
+
+gem 'rest-client'
+gem 'xml-simple'
--- app/models/way.rb
+++ app/models/way.rb
@@ -1,4 +1,18 @@
+require 'rest_client'
+require 'xmlsimple'
+
 class Way < ActiveRecord::Base
   attr_accessible :category, :name
   has_many :nodes, :inverse_of => :way
+
+  def self.fetch_from_OSM(osmid)
+    # Hit OSM API.
+    @body = RestClient.get "http://www.openstreetmap.org/api/0.6/way/#{osmid}"
+    @data = XmlSimple.xml_in(@body)
+    puts @data.inspect
+
+    #Add way info and nodes to new model
+
+    # save model.
+  end
 end
