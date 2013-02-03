fb0fe05c4b0d3c8dd9ffc0560e090f5480e26a38
Calum Halcrow <calumhalcrow@gmail.com>
2013-02-03 17:56:47 +0800

    Working fetch-OSM-way tool.

--- app/models/way.rb
+++ app/models/way.rb
@@ -8,12 +8,24 @@ class Way < ActiveRecord::Base
 
   def self.fetch_from_OSM(osmid)
     # Hit OSM API.
-    @body = RestClient.get "http://www.openstreetmap.org/api/0.6/way/#{osmid}"
-    @data = XmlSimple.xml_in(@body)
-    puts @data.inspect
+    body = RestClient.get "http://www.openstreetmap.org/api/0.6/way/#{osmid}"
+    way_data = XmlSimple.xml_in(body)
 
-    #Add way info and nodes to new model
+    way = Way.create(:category => 'trail')
 
-    # save model.
+    way_data['way'][0]['nd'].each_with_index do |node_data, index|
+      body = RestClient.get "http://www.openstreetmap.org/api/0.6/node/#{node_data['ref']}"
+      node_data = XmlSimple.xml_in(body)
+
+      node = Node.create(
+        :lat => node_data['node'][0]['lat'],
+        :lon => node_data['node'][0]['lon'],
+        :osmid => node_data['node'][0]['id'])
+
+      Position.create(
+        :order => index,
+        :way_id => way.id,
+        :node_id => node.id)
+    end
   end
 end
