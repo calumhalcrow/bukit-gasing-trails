6c604875412c7f388ab7027453e07ec6d1113229
Calum Halcrow <calumhalcrow@gmail.com>
2013-02-09 15:32:32 +0800

    Crude initial map colouring code.
    Refactoring to come!

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -16,27 +16,89 @@ $(function () {
             way.positions.forEach(function(position) {
                 coords.push(new google.maps.LatLng(position.node.lat, position.node.lon));
             });
-            var shape = Shape(way);
-            var options = ShapeOptions(shape);
+            var polyTypeAndOptions = PolyTypeAndOptions(way);
+            var polyType = polyTypeAndOptions[0];
+            var options = polyTypeAndOptions[1];
             options['path'] = coords;
 
-            (new google.maps[shape](options)).setMap(map);
+            var shape = new google.maps[polyType](options);
+            shape.setMap(map);
+
+            makeInfoWindowEvent(map, shape, way);
+
         });
     });
 });
 
-var Shape = function (way) {
-    return (way.category == 'trail') ? 'Polyline' : 'Polygon';
-};
+function makeInfoWindowEvent(map, shape, way) {
+  google.maps.event.addListener(shape, 'click', function(event) {
+    var infoWindow = new google.maps.InfoWindow({
+        content: 'ID: '+way['id'],
+        position: event.latLng
+    });
+    infoWindow.open(map);
+  });
+}
 
-var ShapeOptions = function (shape) {
+var PolyTypeAndOptions = function (way) {
     var options = {};
-    if (shape == 'Polyline') {
+    var polyType;
+    if (way.category == 'trail') {
+        polyType = 'Polyline';
         options = {
-            strokeColor: '#00ff00',
+            strokeColor: '#7C4D27',
             strokeOpacity: 1.0,
-            strokeWeight: 2
+            strokeWeight: 2,
+            zIndex: 10
+        };
+    } else if (way.category == 'boundary') {
+        polyType = 'Polygon';
+        options = {
+            strokeColor: "#196224",
+            strokeOpacity: 0.8,
+            strokeWeight: 1,
+            fillColor: "#196224",
+            fillOpacity: 0.05,
+            clickable: false
+        };
+    } else if (way.category == 'reservoir') {
+        polyType = 'Polygon';
+        options = {
+            strokeColor: "#2E546A",
+            strokeOpacity: 0.5,
+            strokeWeight: 1,
+            fillColor: "#54A9D9",
+            fillOpacity: 0.5,
+            clickable: false
+        };
+    } else if (way.category == 'place') {
+        polyType = 'Polygon';
+        options = {
+            strokeColor: "#7D4281",
+            strokeOpacity: 0.5,
+            strokeWeight: 1,
+            fillColor: "#B440BB",
+            fillOpacity: 0.5,
+            clickable: false
+        };
+    } else if (way.category == 'parking') {
+        polyType = 'Polygon';
+        options = {
+            strokeColor: "#AFA941",
+            strokeOpacity: 0.5,
+            strokeWeight: 1,
+            fillColor: "#F0E328",
+            fillOpacity: 0.5,
+            clickable: false
+        };
+    } else if (way.category == 'road') {
+        polyType = 'Polyline';
+        options = {
+            strokeColor: '#ffffff',
+            strokeOpacity: 1.0,
+            strokeWeight: 4,
+            zIndex: 10
         };
     }
-    return options;
+    return [polyType, options];
 };
