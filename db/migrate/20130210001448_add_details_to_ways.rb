2e52ecf1e598e988eb4f9154ad342b42ac2223d8
Calum Halcrow <calumhalcrow@gmail.com>
2013-03-09 12:39:23 +0800

    Deletes Rails, sets up basic Jekyll site.
    Jekyll site not fully functional yet.

--- db/migrate/20130210001448_add_details_to_ways.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToWays < ActiveRecord::Migration
-  def change
-    add_column :ways, :enabled, :boolean, :default => true
-    add_column :ways, :desc, :text
-    add_column :ways, :osmid, :integer
-  end
-end
--- db/migrate/20130223033039_add_icon_to_positions.rb
+++
@@ -1,6 +0,0 @@
-class AddIconToPositions < ActiveRecord::Migration
-  def change
-    add_column :positions, :icon, :string
-    remove_column :positions, :has_icon, :boolean
-  end
-end
